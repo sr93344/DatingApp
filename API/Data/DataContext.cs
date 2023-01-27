4481478c2b90ee9af4752ea58a096d4c15f3c02f
suduu <sr93344@gmail.com>
2023-01-27 12:35:36 +0530

    End of Section 4

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,4 +1,4 @@
-using API.Entitties;
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,25 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-var app = builder.Build();
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
 
+var app = builder.Build();
 // Configure the HTTP request pipeline
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
